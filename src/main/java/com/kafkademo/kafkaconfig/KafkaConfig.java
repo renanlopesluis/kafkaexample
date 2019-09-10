f9b7feaeaedc6f935bc5aa0db49a598b5fefd67f
Renan Lopes <renan.ilegra@terceiros.zenvia.com>
2019-09-10 14:27:56 -0300

    kafkaConfig refactored

--- src/main/java/com/kafkademo/kafkaconfig/KafkaConfig.java
+++ src/main/java/com/kafkademo/kafkaconfig/KafkaConfig.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.Configuration;
 public class KafkaConfig<T> {
 	
 	protected final String HOST = "127.0.0.1:9092";
+	protected final String USER_GROUP_ID = "group_id";	
 	
-	public final static String USER_TOPIC = "user_kafka";
-	public final static String USER_GROUP_ID = "group_id";
-	
-
-	
+	public static final String USER_TOPIC = "user_kafka";
 }
